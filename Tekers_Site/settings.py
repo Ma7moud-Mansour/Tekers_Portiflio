cc4610093b61e563420a61c6707ec7af76c3de3c
mahmoud <mahmoudabdelkareem.info@gmail.com>
2026-03-21 14:54:54 +0200

    fix bug

--- Tekers_Site/settings.py
+++ Tekers_Site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%e=fzd2e8rnsvubr)8-@%-0bw^izpn2k$y44bdkb%hjmg1a-)_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*] 
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
